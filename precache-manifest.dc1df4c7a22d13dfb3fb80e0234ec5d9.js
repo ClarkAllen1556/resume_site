1db3220ab047bdf39e2645dd141be72b84d03ee7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-23 06:10:43 +0000

    Deploy ClarkAllen1556/resume_site to github.com/ClarkAllen1556/resume_site.git:gh-pages

---
+++ precache-manifest.dc1df4c7a22d13dfb3fb80e0234ec5d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9368a4b2cc0cfe40d31e",
+    "url": "/css/app.916eb131.css"
+  },
+  {
+    "revision": "3ca6640372e52d873935",
+    "url": "/css/chunk-vendors.6217269f.css"
+  },
+  {
+    "revision": "ee47946415b7a65f0180213b0aa7a8e6",
+    "url": "/img/allen_head_1.ee479464.jpeg"
+  },
+  {
+    "revision": "43af92e04ee46641b05bdabb67d15648",
+    "url": "/img/bg_kyushu.43af92e0.jpg"
+  },
+  {
+    "revision": "4f95284b73f837fd3ef0a5fac203fac6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9368a4b2cc0cfe40d31e",
+    "url": "/js/app.40963b34.js"
+  },
+  {
+    "revision": "3ca6640372e52d873935",
+    "url": "/js/chunk-vendors.5e71cfa2.js"
+  },
+  {
+    "revision": "5fb3c1fd7e647d893c124ca2f74859c3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
